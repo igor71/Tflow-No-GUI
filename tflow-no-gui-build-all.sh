13526a2e15c9d5e07dbe2259af851861e904f0bf
igor71 <34439207+igor71@users.noreply.github.com>
2018-11-19 13:30:20 +0200

    Update tflow-no-gui-build-all.sh

--- tflow-no-gui-build-all.sh
+++ tflow-no-gui-build-all.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 EXTRA_FLAGS="--no-cache"
 
-TFLOW_VER=latest
 TFLOW_NO_GUI_VER=latest
 
-docker build $EXTRA_FLAGS -f Dockerfile.Basic -t yi/tflow-no-gui-base:${TFLOW_VER} . && \
-docker build $EXTRA_FLAGS -f Dockerfile.Tflow -t yi/tflow-no-gui:${TFLOW_NO_GUI_VER} .
+docker build $EXTRA_FLAGS -f Dockerfile-tf-3.6 -t yi/tflow-no-gui:${TFLOW_NO_GUI_VER} .
