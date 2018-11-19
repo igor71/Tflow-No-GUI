3d592d735913fd63d070178f8b13b0f7cd0ea8c5
igor71 <34439207+igor71@users.noreply.github.com>
2018-11-19 09:35:26 +0200

    Create tflow-no-gui-build-all.sh

---
+++ tflow-no-gui-build-all.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+EXTRA_FLAGS="--no-cache"
+
+TFLOW_VER=latest
+TFLOW_NO_GUI_VER=latest
+
+docker build $EXTRA_FLAGS -f Dockerfile.tflow -t yi/tflow:${TFLOW_VER} . && \
+docker build $EXTRA_FLAGS -f Dockerfile.tflow-gui -t yi/tflow-gui:${TFLOW_NO_GUI_VER} .
