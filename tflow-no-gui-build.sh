1a4ae62832ee581a1c0eeaa8efa90ce461a42584
igor71 <34439207+igor71@users.noreply.github.com>
2018-11-20 08:21:54 +0200

    Update and rename tflow-no-gui-build-all.sh to tflow-no-gui-build.sh

--- tflow-no-gui-build.sh
+++ tflow-no-gui-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 EXTRA_FLAGS="--no-cache"
 
-TFLOW_NO_GUI_VER=latest
+TFLOW_NO_GUI_VER=1.8.0-python-3.6 
 
 docker build $EXTRA_FLAGS -f Dockerfile-tf-3.6 -t yi/tflow-no-gui:${TFLOW_NO_GUI_VER} .
